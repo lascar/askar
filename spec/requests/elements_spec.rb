9cf7f66fb41602c9fec74d4d8f59ff937376d7e6
lascar <pascal.carrie@gmail.com>
2013-06-28 18:28:47 +0200

    rspec update

--- spec/requests/elements_spec.rb
+++ spec/requests/elements_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-describe 'list add element' do
+describe 'add, show, update and delete element' do
   before(:each) do
     Element.create!(:name => 'element 1')
   end
@@ -13,12 +13,34 @@ describe 'list add element' do
     find('#element_2_name', :visible => true).text.should == "element 2"
   end
 
+  # save_page('capy.page.html')
   # :focus => true
-  it 'lets the user add an new element', :js => true do
+  it 'lets the user show an new element', :js => true do
     visit elements_list_path
-    save_page('capy.page.html')
     find(:xpath, '//a[@href="/elements/show/1"]').text.should == "Show"
     find(:xpath, '//a[@href="/elements/show/1"]').click
     find('#element_1_show_name', :visible => true).text.should == "element 1"
+  end
+  
+  it 'lets the user update an new element from index', :js => true do
+    visit elements_list_path
+    find(:xpath, '//a[@href="/elements/edit/1"]').text.should == "Edit"
+    find(:xpath, '//a[@href="/elements/edit/1"]').click
+    find(:xpath, '//textarea[@id="element_short_description"]')
+    fill_in "element[short_description]", :with => 'primero elemento'
+    click_button('Update Element')
+    find('#element_1_short_description', :visible => true).text.should == "primero elemento"
+  end
+  
+  it 'lets the user update an new element from show', :focus => true, :js => true do
+    visit elements_list_path
+    find(:xpath, '//a[@href="/elements/show/1"]').click
+    find(:xpath, '//a[@id="element_1_show_edit_link"]').click
+    find('#element_short_description')
+    fill_in "element[short_description]", :with => 'primero elemento'
+    click_button('Update Element')
+    find('#element_1_show_short_description', :visible => true).text.should == "primero elemento"
+    find('#tab_elements_list').click
+    find('#element_1_short_description', :visible => false ).text.should == 'primero elemento'
   end
 end
