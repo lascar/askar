a26a9b611d4002b362016eb7ce900291f1319448
pascal <pascal.carrie@gmail.com>
2013-11-29 14:53:20 +0100

    quit jquery (still is ajax)

--- app/assets/javascripts/lascar.js
+++ app/assets/javascripts/lascar.js
@@ -25,13 +25,12 @@ var Lascar = {
 
   displayTab: function () {
     'use strict';
-    var tab_width, action = Lascar.action === "show" ? " " + Lascar.element.id : "",
-      suffix = Lascar.action + action,
+    var tab_width, suffix = Lascar.action === "show" ? "_" + Lascar.element.id : "",
       tab = Lascar.createOrActiveNode("tab_", suffix, "tabs");
-    tab.addClass("tab");
+    tab.setAttribute('class', 'tab active');
     tab_width = Lascar.controller.length > suffix.length ? Lascar.controller.length + 10 : suffix.length + 10;
     // set the width for ie78
-    tab.html(Lascar.controller + "<br>" + suffix);
+    tab.innerHTML = Lascar.controller + "<br>" + Lascar.action + suffix;
   },
 
   createDiv: function (id_name, parent_id, class_name, content) {
@@ -74,58 +73,57 @@ var Lascar = {
     Lascar.desactiveChildren(parent_id);
     node = document.getElementById(id) || this.createDiv(id, parent_id);
     // for ie7-8
-    class_names = node.className ? node.className : "";
-    if (/inactive/.test(class_names)){
+    class_names = node.className || "";
+    if (/inactive/.test(class_names)) {
       Lascar.toogleClass(node, "inactive", "active");
     } else {
       node.className = class_names + " active";
     }
-    return $(node);
+    return node;
   },
 
   displayTabContentShow: function () {
     'use strict';
-    var tab_content, i, field, label_field_raw_id, text_field_raw_id;
+    var tab_content, i, field, field_raw_id, field_raw, label_field_raw_id, text_field_raw_id;
     Lascar.desactiveChildren("tabs_contents");
     tab_content =  Lascar.createOrActiveNode("", "", "tabs_contents");
-    tab_content.addClass("tab_content");
+    tab_content.className += ' tab_content';
     for (i = 0; i < Lascar.fields_to_show.length; i += 1) {
       field = Lascar.fields_to_show[i];
-      console.log(field);
-      var field_raw_id = Lascar.controller + "_" + Lascar.action + "_" + Lascar.element.id + "_" + field;
+      field_raw_id = Lascar.controller + "_" + Lascar.action + "_" + Lascar.element.id + "_" + field;
       label_field_raw_id = Lascar.controller + "_" + Lascar.action + "_" + Lascar.element.id + "_" + field + "_label";
       text_field_raw_id =  Lascar.controller + "_" + Lascar.action + "_" + Lascar.element.id + "_" + field + "_text";
-      var field_raw = Lascar.createDiv(field_raw_id, tab_content.attr('id'), "field_raw " + field);
+      field_raw = Lascar.createDiv(field_raw_id, tab_content.id, "field_raw " + field);
       Lascar.createDiv(label_field_raw_id, field_raw_id, "label " + field, field);
       Lascar.createDiv(text_field_raw_id, field_raw_id, "text " + field, Lascar.element[field]);
-    };
+    }
   },
 
   displayTabContentList: function () {
     'use strict';
     var i, j, element, field, action, element_raw, field_content_div, link_action,
       tab_content = Lascar.createOrActiveNode("", "", "tabs_contents");
-    tab_content.addClass('tab_content');
-    for (i = 0; i < Lascar.elements.length; i +=1) {
+    tab_content.className += ' tab_content';
+    for (i = 0; i < Lascar.elements.length; i += 1) {
       element = Lascar.elements[i];
-      element_raw = $(Lascar.createDiv("element_raw_" + element.id, tab_content.attr('id'), "element_raw"));
-      for (j = 0; j < Lascar.fields_to_show.length; j += 1){
+      element_raw = Lascar.createDiv("element_raw_" + element.id, tab_content.id, "element_raw");
+      for (j = 0; j < Lascar.fields_to_show.length; j += 1) {
         field = Lascar.fields_to_show[j];
-        field_content_div = $(Lascar.createDiv(Lascar.controller + "_" + Lascar.action + "_" + element.id, element_raw.attr('id'), "field " + field, element[field]));
-      };
-      for (j = 0; j < Lascar.actions.length; j += 1){
+        field_content_div = Lascar.createDiv(Lascar.controller + "_" + Lascar.action + "_" + element.id, element_raw.id, "field " + field, element[field]);
+      }
+      for (j = 0; j < Lascar.actions.length; j += 1) {
         action = Lascar.actions[j];
-        Lascar.buildLinkAction(element, action, element_raw.attr('id'));
-      };
-    };
+        Lascar.buildLinkAction(element, action, element_raw.id);
+      }
+    }
   },
 
   buildLinkAction: function (element, action, parent_id) {
     'use strict';
     var div_action, link_action;
-    div_action = $(Lascar.createDiv("div_link_" + Lascar.controller + "_" + action + "_" + element.id, parent_id, "div_link_" + action + " field"));
-    link_action = $(Lascar.createDiv("link_" + Lascar.controller + "_" + action + "_" + element.id, div_action.attr('id'), "link_" + action, action));
-    link_action.on('click', function () { Lascar.lauchAction(element.id, action); });
+    div_action = Lascar.createDiv("div_link_" + Lascar.controller + "_" + action + "_" + element.id, parent_id, "div_link_" + action + " field");
+    link_action = Lascar.createDiv("link_" + Lascar.controller + "_" + action + "_" + element.id, div_action.id, "link_" + action, action);
+    $(link_action).on('click', function () { Lascar.lauchAction(element.id, action); });
   },
 
   lauchAction: function (element_id, action) {
