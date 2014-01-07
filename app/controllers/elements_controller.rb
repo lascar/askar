097c4f1cc20c20edaba94886aa3c89438d9ab71a
pascal <pascal.carrie@gmail.com>
2014-01-07 11:36:40 +0100

    all rest

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -2,17 +2,66 @@ class ElementsController < ApplicationController
   respond_to :json
   PERMITED_FIELDS = ["id", "name", "description"]
   DEFAULT_FIELDS = ["id", "name", "description"]
+  before_action :set_element, only: [:show, :edit, :update, :destroy]
   before_action :field_names_can_be
 
+  # GET /elements
   def index
     respond_with(@elements = Element.all)
   end
 
+  # GET /elements/1
   def show
     respond_with(@element = Element.find(params[:id]))
   end
 
+  # GET /elements/new
+  def new
+    respond_with(@element = Element.new)
+  end
+
+  # GET /elements/1/edit
+  def edit
+  end
+
+  # POST /elements
+  def create
+    @element = Element.new(element_params)
+
+    if @element.save
+      redirect_to @element, notice: 'Element was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  # PATCH/PUT /elements/1
+  def update
+    if @element.update(element_params)
+      redirect_to @element, notice: 'Element was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  # DELETE /elements/1
+  def destroy
+    @element.destroy
+    redirect_to elements_url, notice: 'Element was successfully destroyed.'
+  end
+
+
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_element
+      @element = Element.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def element_params
+      params.require(:element).permit(:name, :description)
+    end
+
     def field_names_can_be
       unless params[:fields] and (PERMITED_FIELDS & params[:fields]).size == params[:fields].size
         params[:fields] = DEFAULT_FIELDS
