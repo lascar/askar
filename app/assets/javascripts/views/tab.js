4c86c2386b7013601d9d1c05fc2ba8be54f54176
lascar <pascal.carrie@gmail.com>
2014-01-07 22:36:17 +0100

    tabs begin

---
+++ app/assets/javascripts/views/tab.js
@@ -0,0 +1,16 @@
+FichaTab = Backbone.View.extend({
+  template: _.template(tabTemplate),
+  initialize: function () {
+    this.model.bind('click', this.render, this);
+    this.model.bind('destroy', this.remove, this);
+  },
+  render: function () {
+    $(this.el).html(this.template(this.model.toJSON()));
+    return this;
+  },
+  remove: function () { /* ... */}
+});
+var elements = new Elements;
+var tab = elements.first;
+var ficha = new FichaTab({el:$('tabs'), model: tab});
+ficha.render();
